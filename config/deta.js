98133adef24474cd112197cf8311c2438986b62d
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2023-11-03 20:58:47 +0700

    Improve login security and simplify code (#13)
    * create login but with little to no security
    
    * Add public routes to spacefile and remove admin login page
    
    * Fix DETA_PROJECT_KEY default value and simplify
    fetch calls.

--- config/deta.js
+++ config/deta.js
@@ -1,4 +1,4 @@
-export const DETA_PROJECT_KEY = process.env.DETA_PROJECT_KEY;
+export const DETA_PROJECT_KEY = process.env.DETA_PROJECT_KEY || "";
 export const DETA_PROJECT_ID = DETA_PROJECT_KEY.split("_")[0];
 export const DETA_BASE_URL = `https://database.deta.sh/v1/${DETA_PROJECT_ID}`;
 export const DETA_DRIVE_URL = `https://drive.deta.sh/v1/${DETA_PROJECT_ID}`;
--- repositories/shop.js
+++ repositories/shop.js
@@ -6,8 +6,7 @@ export const Shop = {
    * @returns {Promise<Shop>}
    */
   get: (key) =>
-    fetch({
-      url: `${DETA_BASE_URL}/shops/items/${key}`,
+    fetch(`${DETA_BASE_URL}/shops/items/${key}`, {
       method: "GET",
       headers: DETA_HEADERS,
       next: { tags: ["shops"] },
@@ -18,8 +17,7 @@ export const Shop = {
    * @returns {Promise<PutResponse<Shop>>}
    */
   put: (items) =>
-    fetch({
-      url: `${DETA_BASE_URL}/shops/items`,
+    fetch(`${DETA_BASE_URL}/shops/items`, {
       method: "PUT",
       headers: DETA_HEADERS,
       body: JSON.stringify({ items }),
