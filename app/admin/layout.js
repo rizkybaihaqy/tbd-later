3ccbcaf145db001aaa077e29ff5e960407254810
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2023-11-03 20:55:05 +0700

    Add public routes to spacefile and remove admin login page

--- app/admin/layout.js
+++
@@ -1,6 +0,0 @@
-import { cookies } from "next/headers";
-
-export default function AdminLayout({ dashboard, login }) {
-  const isLoggedIn = cookies().get("auth").value === "true";
-  return isLoggedIn ? dashboard : login;
-}
