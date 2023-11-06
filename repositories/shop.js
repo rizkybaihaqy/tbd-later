0e24e8e7744c309f6ff24b582ce750fdb172864b
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2023-11-06 20:14:02 +0700

    Refactor Deta Base api

--- repositories/shop.js
+++ repositories/shop.js
@@ -1,26 +1,10 @@
-import { DETA_HEADERS, DETA_BASE_URL } from "@/config/deta";
+import { Base } from "@/lib/deta/base.js";
 
-export const Shop = {
-  /**
-   * @param {String} key
-   * @returns {Promise<Shop>}
-   */
-  get: (key) =>
-    fetch(`${DETA_BASE_URL}/shops/items/${key}`, {
-      method: "GET",
-      headers: DETA_HEADERS,
-      next: { tags: ["shops"] },
-    }).then((res) => res.json()),
-
-  /**
-   * @param {Shop[]} items
-   * @returns {Promise<PutResponse<Shop>>}
-   */
-  put: (items) =>
-    fetch(`${DETA_BASE_URL}/shops/items`, {
-      method: "PUT",
-      headers: DETA_HEADERS,
-      body: JSON.stringify({ items }),
-      cache: "no-store",
-    }).then((res) => res.json()),
-};
+export const Shop = Base({
+  name: "shops",
+  property: {
+    key: "",
+    name: "",
+    password: "",
+  },
+});
--- types/deta.js
+++ types/deta.js
@@ -5,4 +5,23 @@
  * @property {T[]} processed.items
  * @property {Object} failed
  * @property {T[]} failed.items
+ * @property {String[]} errors
+ */
+
+/**
+ * @template T
+ * @typedef {Object} UpdateResponse
+ * @property {Object} key
+ * @property {T} set
+ * @property {String[]} errors
+ */
+
+/**
+ * @template T
+ * @typedef {Object} QueryResponse
+ * @property {Object} paging
+ * @property {Number} paging.size
+ * @property {String} paging.last
+ * @property {T[]} items
+ * @property {String[]} errors
  */
