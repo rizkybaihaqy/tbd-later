a29df6074e201e2160d215e43cde7bb205b8cf06
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2023-11-08 16:20:52 +0700

    Export BASE_URL and ASSET_URL in app.js

--- config/app.js
+++ config/app.js
@@ -1,2 +1,2 @@
-const BASE_URL = process.env.BASE_URL || "http://localhost:4200";
-const ASSET_URL = BASE_URL + "/assets";
+export const BASE_URL = process.env.BASE_URL || "http://localhost:4200";
+export const ASSET_URL = BASE_URL + "/assets";
