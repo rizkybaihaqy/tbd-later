7bfd00eb00bebed9fd39f002afc8f7164d80f010
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2023-11-08 16:21:34 +0700

    Refactor shop retrieval and display

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
-import { Shop } from "@/repositories/shop.js";
+import { ShopService } from "@/services/shop.js";
 import Image from "next/image.js";
 import { redirect } from "next/navigation.js";
 
 export default async function Home() {
-  const shop = await Shop.get("shop");
-  if (!shop.name) {
+  const response = await ShopService.retrieve("shop");
+  if (!response.success && "errors" in response) {
     redirect("/setup");
   }
+
+  const shop = "data" in response && response.data;
 
   const menus = [
     ["1", "2", "3"],
@@ -17,12 +19,7 @@ export default async function Home() {
   return (
     <main className="container">
       <section>
-        <Image
-          src={"http://localhost:4200/assets/logo.png"}
-          alt="logo"
-          width={100}
-          height={100}
-        />
+        <Image src={shop.logo} alt="logo" width={100} height={100} />
         <h1>{shop.name}</h1>
       </section>
       {menus.map((menu, i) => (
